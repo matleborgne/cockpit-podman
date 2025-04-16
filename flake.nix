2efec599d93aeb6bf546e253bbc3ff3f30685c21
Mathieu <mathieu.leborgne.pro@pm.me>
2025-04-16 23:44:57 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "cockpit-project/cockpit-podman app";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        #"aarch64-linux"
+        #"x86_64-darwin"
+        #"aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f  {
+        pkgs = import nixpkgs { inherit system; };
+      });
+
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+
+        default = pkgs.stdenv.mkDerivation rec {        
+          name = "cockpit-podman";
+          src = self;
+
+          nativeBuildInputs = with pkgs; [
+            gettext
+            nodejs
+            git
+            gawk
+          ];
+
+          makeFlags = [ "DESTDIR=$(out)" "PREFIX=" ];
+
+          postPatch = ''
+            substituteInPlace Makefile --replace-quiet /usr/local/share $out/share
+
+            mkdir -p pkg/lib
+            mkdir -p dist
+            
+            touch pkg/lib/cockpit-po-plugin.js
+            touch dist/manifest.json
+          '';
+
+          dontBuild = true;
+ 
+        };
+      });
+    };
+}
